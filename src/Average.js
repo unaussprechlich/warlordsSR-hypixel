b74ace817f70d375439a65656be50ffead33c11d
unaussprechlich <alexander.keck@unaussprechlich.net>
2020-04-19 00:30:30 +0200

    200k Max dhp for aqua, prot, spirit, earth

--- src/Average.js
+++ src/Average.js
@@ -10,13 +10,13 @@ class Average {
 exports.Average = Average;
 exports.PYROMANCER = new Average(0, 90000, 1);
 exports.CRYOMANCER = new Average(0, 90000, 1);
-exports.AQUAMANCER = new Average(0, 90000, 1);
+exports.AQUAMANCER = new Average(0, 100000, 1);
 exports.AVENGER = new Average(0, 90000, 1);
 exports.CRUSADER = new Average(0, 90000, 1);
-exports.PROTECTOR = new Average(0, 90000, 1);
+exports.PROTECTOR = new Average(0, 100000, 1);
 exports.THUNDERLORD = new Average(0, 90000, 1);
-exports.SPIRITGUARD = new Average(0, 90000, 1);
-exports.EARTHWARDEN = new Average(0, 90000, 1);
+exports.SPIRITGUARD = new Average(0, 100000, 1);
+exports.EARTHWARDEN = new Average(0, 100000, 1);
 exports.BERSERKER = new Average(0, 90000, 1);
 exports.DEFENDER = new Average(0, 90000, 1);
 exports.REVENANT = new Average(0, 90000, 1);
--- src/Average.ts
+++ src/Average.ts
@@ -13,13 +13,13 @@ export class Average{
 
 export const PYROMANCER = new Average(0, 90000, 1);
 export const CRYOMANCER = new Average(0, 90000 , 1);
-export const AQUAMANCER = new Average(0, 90000, 1);
+export const AQUAMANCER = new Average(0, 100000, 1);
 export const AVENGER    = new Average(0, 90000, 1);
 export const CRUSADER   = new Average(0, 90000 , 1);
-export const PROTECTOR  = new Average(0, 90000, 1);
+export const PROTECTOR  = new Average(0, 100000, 1);
 export const THUNDERLORD= new Average(0, 90000, 1);
-export const SPIRITGUARD= new Average(0, 90000, 1);
-export const EARTHWARDEN= new Average(0, 90000, 1);
+export const SPIRITGUARD= new Average(0, 100000, 1);
+export const EARTHWARDEN= new Average(0, 100000, 1);
 export const BERSERKER  = new Average(0, 90000 , 1);
 export const DEFENDER   = new Average(0, 90000 , 1);
 export const REVENANT   = new Average(0, 90000, 1);
